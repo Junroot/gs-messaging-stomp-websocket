5576573592adaf3d2cf76a56f1d28166f93a3248
ROOT <junroot0909@gmail.com>
2021-07-12 22:37:38 +0900

    feat: 방 여러 개 가능하도록 구현

--- complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -1,19 +1,25 @@
 package com.example.messagingstompwebsocket;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
 
+	private final SimpMessagingTemplate template;
 
-	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
-	public Greeting greeting(HelloMessage message) throws Exception {
-		Thread.sleep(1000); // simulated delay
-		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	public GreetingController(SimpMessagingTemplate template) {
+		this.template = template;
 	}
 
+	@MessageMapping("/rooms/{roomId}")
+	public void greeting(@DestinationVariable String roomId, HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		Greeting greeting = new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+		template.convertAndSend("/chatting-response/" + roomId, greeting);
+	}
 }
